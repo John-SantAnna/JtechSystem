765568cc6137667e20b62aeaef9dbfcd4e4c584f
John-SantAnna <john.santanna@outlook.com>
2021-01-13 23:48:21 -0300

    INICIO - Projeto vazio, MVC, NET 3-1

--- JtechSystem/Program.cs
+++ JtechSystem/Program.cs
@@ -20,7 +20,7 @@ namespace JtechSystem
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>();
+                    webBuilder.UseStartup<Startup>(); 
                 });
     }
 }
